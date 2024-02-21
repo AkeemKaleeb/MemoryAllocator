94a77858be37d81a9497c4a30d9e5c7286bf94a6
shreyash618 <115910588+shreyash618@users.noreply.github.com>
2024-02-21 17:48:53 -0500

    Update memtest.c
    added a few print statements

--- memtest.c
+++ memtest.c
@@ -9,33 +9,39 @@
 #include "mymalloc.h"
 #endif
 
-// malloc() and immediately free() a 1-byte object, 120 times
-void test1() {
-    for(int i = 0; i < 120; i++) {
-        void* ptr = malloc(1);
-        printf("obj[%d] is allocated: %s\n", i, ((char *)ptr - 16));
+#define MEMSIZE 640
+#define HEADERSIZE 16
+#define OBJECTS 10
+#define OBJSIZE (MEMSIZE / OBJECTS - HEADERSIZE)
 
-        free(ptr);
-        printf("obj[%d] is freed: %s\n", i, ((char *)ptr - 16));
-    }
-}
-
-// Use malloc() to get 120 1-byte objects, storing the pointers in an array, then use free() to deallocate the chunks
-void test2() {
-    char* obj[120];
-
-    for (int i = 0; i < 10; i++) {
-        obj[i] = malloc(1);
-        printf("obj[%d] is allocated: %s\n", i,((char *)obj[i] - 16));
-    }
-
-    for(int i = 0; i < 10; i++) {
-        free(obj[i]);
-        printf("obj[%d] is freed: %s\n", i,((char *)obj[i] - 16));
-    }
-}
 int main(int argc, char **argv)
 {
-    test1();
-    test2();
+	printf("Objsize is %d bytes\n", OBJSIZE); //prints out the number of bytes in each object 
+	char *obj[OBJECTS]; 
+	int i, j, errors = 0;
+	
+	// fill memory with objects
+	for (i = 0; i < OBJECTS; i++) {
+		printf ("\n\nMallocing object no. %d!\n", i);
+		obj[i] = malloc(OBJSIZE); //mallocing each object in the array
+	}
+	
+	// fill each object with distinct bytes
+	for (i = 0; i < OBJECTS; i++) {
+		memset(obj[i], i, OBJSIZE); //sets obj[i] = i (with OBJSIZE bytes)
+	}
+	
+	// check that all objects contain the correct bytes
+	for (i = 0; i < OBJECTS; i++) {
+		for (j = 0; j < OBJSIZE; j++) {
+			if (obj[i][j] != i) {
+				errors++;
+				printf("Object %d byte %d incorrect: %d\n", i, j, obj[i][j]);
+			}
+		}
+	}
+	
+	printf("%d incorrect bytes\n", errors);
+	
+	return EXIT_SUCCESS;
 }
