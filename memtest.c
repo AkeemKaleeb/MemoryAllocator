d0d4f3585509d11cc4d8299c26086e618411c64e
shreyash618 <115910588+shreyash618@users.noreply.github.com>
2024-02-22 17:41:38 -0500

    Update memtest.c

--- memtest.c
+++ memtest.c
@@ -1,3 +1,9 @@
+// Shreya Shukla & Kaileb Cole
+// CS 214: Systems Programming
+// Professor Menendez
+// Programming Assignment #1
+// 02-19-2024
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,25 +15,28 @@
 #include "mymalloc.h"
 #endif
 
-#define MEMSIZE 512
+#define MEMSIZE 4096 //640
 #define HEADERSIZE 16
-#define OBJECTS 16
+#define OBJECTS 64
 #define OBJSIZE (MEMSIZE / OBJECTS - HEADERSIZE)
+//objsize is 49.2 ->54
 
 int main(int argc, char **argv)
 {
-	char *obj[OBJECTS];
+	//printf("Objsize is %d bytes\n", OBJSIZE); //prints out the number of bytes in each object 
+	char *obj[OBJECTS]; 
 	int i, j, errors = 0;
 	
 	// fill memory with objects
 	for (i = 0; i < OBJECTS; i++) {
-		obj[i] = malloc(OBJSIZE);
+		//printf("\n*****************************************************************************************************************************************");
+		//printf ("\n\nMallocing object no. %d!\n", i);
+		obj[i] = malloc(OBJSIZE); //mallocing each object in the array
 	}
 	
 	// fill each object with distinct bytes
 	for (i = 0; i < OBJECTS; i++) {
-		memset(obj[i], i, OBJSIZE);
-		printf("obj[%d]: %d\n", i, *obj[i]);
+		memset(obj[i], i, OBJSIZE); //sets obj[i] = i (with OBJSIZE bytes)
 	}
 	
 	// check that all objects contain the correct bytes
@@ -41,6 +50,6 @@ int main(int argc, char **argv)
 	}
 	
 	printf("%d incorrect bytes\n", errors);
-
+	
 	return EXIT_SUCCESS;
 }
